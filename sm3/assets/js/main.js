60c8d7d4b166ab8314a57cf35b4cf369881b5c34
Тимофеева Елена Александровна <242019@mck-ktits.ru>
2023-10-17 13:45:58 +0300

    sm3

---
+++ sm3/assets/js/main.js
@@ -0,0 +1,54 @@
+let accordionArr = [
+    {
+        name: 'С какого возраста к нам можно?',
+        text: 'Самые юные райдеры у нас 10 лет. Важно, что до 16 лет мы берем только со взрослыми под их ответственность. также есть ограничения по весу: от 40 кг до 100 кг '
+    },
+    {
+        name: 'Сколько по времени длится катание?',
+        text: 'Продолжительность одной поездки составляет 2 часа (с учетом переодевания в обе стороны).'
+    },
+    {
+        name: 'Нужны ли права А-категории для управления мотоциклом?',
+        text: 'Для аренды мотоцикла у нас, на прокат или тренировку категория А не нужна, потому что занятия проходят на специально подготовленных трассах, и все маршруты проложены вне дорог общественного пользования.'
+    },
+    {
+        name: 'Отвечаете ли вы за здоровье клиентов?',
+        text: 'Мы отвечаем за исправность выдаваемой мототехники, правильно подобранную по росту экипировку, годами отработанную тренировочную программу - за все то, что обеспечивает пассивную безопасность клиента. За свое здоровье может отвечать только сам человек за рулем.'
+    },
+];
+
+// console.log(accordion)
+
+let accordion = document.querySelector('.accordion')
+
+for(let i = 0; i<accordionArr.length; i++){
+    let accordionContent = document.createElement('div')
+    accordionContent.className = 'accordion-content'
+    accordion.append(accordionContent)
+
+    let accordionName = document.createElement('div')
+    accordionName.className = 'accordion-name'
+    accordionName.innerHTML = accordionArr[i].name
+    accordionContent.append(accordionName)
+
+    let accordionText = document.createElement('div')
+    accordionText.className = 'accordion-text'
+    accordionText.innerHTML = accordionArr[i].text
+    accordionContent.append(accordionText)
+}
+
+let accordionElements = document.querySelectorAll('.accordion-content')
+
+for(let item of accordionElements){
+    item.onclick = function(event){
+
+        let target = event.target.closest('.accordion-name')
+
+        if(!target) return
+
+        let text = item.querySelector('.accordion-text')
+
+        text.classList.toggle('active')
+        target.classList.toggle('open')
+    }
+}
